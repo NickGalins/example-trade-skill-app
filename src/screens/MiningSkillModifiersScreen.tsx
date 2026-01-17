c105f491bd52daf5479d33c7ae9982008a345ce2
RazorATX <nick.galinski@gmail.com>
2026-01-17 16:32:21 -0600

    Fix remaining React imports in screen components
    - EventPlayerInfoScreen: useState import only
    - MiningSkillModifiersScreen: useState, useEffect imports only

--- src/screens/MiningSkillModifiersScreen.tsx
+++ src/screens/MiningSkillModifiersScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from '../components/Button';
 import { Select } from '../components/Select';
 import { ToggleSwitch } from '../components/ToggleSwitch';
